0fcc4a286f2b9f1cea90e2346418fb98c118d3fa
Caroly Vilo <70900275+carolyv@users.noreply.github.com>
2022-02-12 17:25:40 +0200

    Kodutöö 1

---
+++ 01_funktsioon/OrbitalSpeed.java
@@ -0,0 +1,20 @@
+// Programm, mis arvutab valja orbitaalse kiiruse Maa suhtes
+
+public class OrbitalSpeed {
+    
+    public static double getOrbitalSpeed(double radius) {
+        double gravitationalConstant = 6.67408 * Math.pow(10, -11);
+        double earthMass = 5.972 * Math.pow(10, 24);
+        if(radius < 6371) {
+            radius = radius + 6371; // Maa raadius
+        }
+        double radiusInMeters = radius * 1000;
+        double orbitSpeed = Math.sqrt((gravitationalConstant * earthMass) / radiusInMeters);
+        return orbitSpeed;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Kuu tiirleb kiirusel " + String.format("%.3f", getOrbitalSpeed(384400)) + " m/s");
+        System.out.println("Rahvusvaheline kosmosejaam tiirleb kiirusel " + String.format("%.3f", getOrbitalSpeed(408)) + " m/s");
+    }
+}
