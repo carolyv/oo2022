959a18d902dd0650ae3f46baba62ee5575993b94
Caroly Vilo <70900275+carolyv@users.noreply.github.com>
2022-05-20 09:30:17 +0300

    Delete Keskmine.java

--- 04_proovikontrolltoo1/Keskmine.java
+++
@@ -1,7 +0,0 @@
-public class Keskmine {
-    /* Koosta funktsioon kolme arvu aritmeetilise keskmise leidmiseks. Katseta. */ 
-    public static float kolmeKeskmine(int a, int b, int c) {
-        float vastus = (a + b + c) / 3f;
-        return vastus;
-    }
-}
